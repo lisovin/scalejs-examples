49cd403f7df3a956bf0c17d4da3d6effa22545bc
Peter Lisovin <peter@lisovin.com>
2013-01-27 23:20:45 -0500

    Panorama example.

---
+++ ModernUI/ModernUI/app/main/bindings/mainBindings.js
@@ -0,0 +1,29 @@
+﻿/*global define */
+/*jslint sloppy: true*/
+define({
+    'main-panorama': function () {
+        return {
+            panorama: {
+                title: 'My Title',
+                pages: [
+                    {
+                        title: 'Page 1',
+                        template: 'main_panorama_page1_template'
+                    },
+                    {
+                        title: 'Page 2',
+                        route: {
+                            view: 'page',
+                            params: '2'
+                        },
+                        template: 'main_panorama_page2_template'
+                    },
+                    {
+                        title: 'Page 3',
+                        template: 'main_panorama_page3_template'
+                    }
+                ]
+            }
+        };
+    }
+});
