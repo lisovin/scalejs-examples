1495ecfbb39f5d9d1bd03edae03cb8d1f367a676
Peter Lisovin <peter@lisovin.com>
2013-12-20 20:02:23 -0500

    Minesweeper initial revision.

---
+++ Sweeper300/Sweeper300/app/main/model/mainModel.js
@@ -0,0 +1,108 @@
+﻿/*global define */
+define([
+    'sandbox!main'
+], function (
+    sandbox
+) {
+    'use strict';
+
+    return function () {
+        var // imports
+            enumerable = sandbox.linq.enumerable;
+
+        function mines(rows, cols, minesNumber) {
+            var minesIndexes = enumerable
+                .generate(function () {
+                    // random 0 - inclusive, 1 - exclusive
+                    return Math.floor(Math.random() * rows * cols);
+                })
+                .distinct()
+                .take(minesNumber)
+                .toArray();
+
+            return minesIndexes;
+        }
+
+        function cells(rows, cols) {
+            var cells = enumerable
+                .range(0, rows)
+                .selectMany(function (row) {
+                    return enumerable
+                        .range(0, cols)
+                        .select(function (col) {
+                            return {
+                                row: row,
+                                col: col,
+                                adjMines: 0
+                            }
+                        });
+                })
+                .toArray();
+
+            return cells;
+        }
+
+        function cellsAsGrid(cells) {
+            var grid = cells
+                .groupBy('$.row')
+                .select(function (g) {
+                    return g.toArray();
+                })
+                .toArray();
+
+            return grid;
+        }
+
+        function neighbours(rows, cols, index) {
+            var row,
+                col, 
+                indexes;
+
+            row = Math.floor(index / cols);
+            col = index % cols;
+            indexes = [
+                    // above
+                    { row: row - 1, col: col - 1 },
+                    { row: row - 1, col: col },
+                    { row: row - 1, col: col + 1 },
+                    // same row
+                    { row: row, col: col - 1 },
+                    { row: row, col: col + 1 },
+                    // below
+                    { row: row + 1, col: col - 1 },
+                    { row: row + 1, col: col },
+                    { row: row + 1, col: col + 1 },
+                ].filter(function (c) {
+                    return c.row >= 0 && c.row < rows &&
+                           c.col >= 0 && c.col < cols;
+                }).map(function (c) {
+                    return c.row * cols + c.col;
+                });
+            
+            return indexes;
+        }
+
+        function board(rows, cols, minesNumber) {
+            var minesIndexes,
+                boardCells;
+
+            minesIndexes = mines(rows, cols, minesNumber);
+            boardCells = cells(rows, cols);
+
+            minesIndexes.forEach(function (index) {
+                neighbours(rows, cols, index).forEach(function (i) {
+                    boardCells[i].adjMines += 1;
+                });
+                
+                boardCells[index].isMine = true;
+            });
+
+            return boardCells;
+        }
+
+        return {
+            board: board,
+            neighbours: neighbours
+        };
+    };
+});
---
+++ Sweeper300/Sweeper300/app/main/viewmodels/mainViewModel.js
@@ -0,0 +1,119 @@
+﻿/*global define */
+define([
+    'sandbox!main',
+    'app/main/model/mainModel',
+    'app/main/viewmodels/cellViewModel'
+], function (
+    sandbox,
+    mainModel,
+    cellViewModel
+) {
+    'use strict';
+
+    return function () {
+        var // imports
+            observable = sandbox.mvvm.observable,
+            observableArray = sandbox.mvvm.observableArray,
+            computed = sandbox.mvvm.computed,
+            enumerable = sandbox.linq.enumerable,
+            // properties
+            model = mainModel(),
+            rows = observable(10),
+            cols = observable(30),
+            totalMines = observable(8),
+            mines = observable(totalMines()),
+            boardCells = [],
+            blewUp = observable(false),
+            result = observable(''),
+            board = observableArray();
+
+        function openAll() {
+            boardCells.forEach(function (cell) {
+                cell.closed(false);
+            });
+        }
+
+        function blowUp() {
+            blewUp(true);
+            openAll();
+        }
+
+        function checkWon() {
+            var allOpenOrFlagged = boardCells.all(function (cell) {
+                return !cell.closed() || cell.isFlag();
+            });
+
+            if (allOpenOrFlagged) {
+                result('You won!');
+            }
+        }
+
+        function open(row, col) {
+            function loop(row, col) {
+                var index,
+                    notMines;
+
+                index = cols() * row + col;
+
+                notMines = model.neighbours(rows(), cols(), index)
+                    .map(function (i) {
+                        return boardCells[i];
+                    })
+                    .filter(function (cell) {
+                        return cell.closed() && !cell.isFlag() && !cell.isMine;
+                    });
+
+                notMines.forEach(function (cell) {
+                    cell.closed(false);
+                    if (cell.adjMines === 0) {
+                        loop(cell.row, cell.col);
+                    }
+                });
+            }
+
+            loop(row, col);
+            checkWon();
+        }
+
+        function createBoard() {
+            boardCells = model
+                .board(rows(), cols(), mines())
+                .map(function (cell) {
+                    return cellViewModel(cell, {
+                        mines: mines,
+                        blowUp: blowUp,
+                        open: open,
+                        checkWon: checkWon
+                    });
+                });
+
+            var newBoard = boardCells
+                .groupBy('$.row')
+                .select(function (g) {
+                    return g.toArray();
+                })
+                .toArray();
+
+            board(newBoard);
+        }
+
+        function reset() {
+            result('');
+            mines(totalMines());
+            blewUp(false);
+            createBoard();
+        }
+
+        reset();
+
+        return {
+            rows: rows,
+            cols: cols,
+            board: board,
+            mines: mines,
+            totalMines: totalMines,
+            reset: reset,
+            result: result
+        };
+    };
+});
