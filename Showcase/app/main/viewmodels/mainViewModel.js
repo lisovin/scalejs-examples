f3deb2dbbac153183a22732a75957ea95f0e3dd9
egucciar <egucciar@gmail>
2014-03-28 18:32:27 -0400

    commit

--- Showcase/app/main/viewmodels/mainViewModel.js
+++ Showcase/app/main/viewmodels/mainViewModel.js
@@ -144,7 +144,7 @@ define([
 
         //createNumberedTiles();
         //createRandomTiles(10);
-        createRandomNumberTiles(100);
+        createRandomNumberTiles(10);
         //createOrderedTiles();
 
         date.subscribe(function (d) {
